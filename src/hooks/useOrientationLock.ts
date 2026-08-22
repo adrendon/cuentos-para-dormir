5df64fde67c72182fcae20275e43d3ee7f885f34
Alexis Rendon <arendon@bancolombia.com.co>
2026-08-21 22:49:26 -0500

    Remove unused useOrientationLock hook (inlined per-screen instead)

--- src/hooks/useOrientationLock.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-import * as ScreenOrientation from 'expo-screen-orientation';
-
-/** Locks the screen orientation while a screen is mounted, restores default on unmount. */
-export function useOrientationLock(orientation: ScreenOrientation.OrientationLock) {
-  useEffect(() => {
-    ScreenOrientation.lockAsync(orientation);
-  }, [orientation]);
-}
